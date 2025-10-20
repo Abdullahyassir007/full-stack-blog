73372165d99108bcdec6ddb9ec78afef9103fe7e
Abdullah yassir A <abdullah2210538@ssn.edu.in>
2025-10-20 16:19:15 +0530

    Switched to yarn

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,6 +1,38 @@
+import { useUser } from "@clerk/clerk-react"
+// import 'react-quill/dist/quill.snow.css';
+
 const Write = () => {
+
+  const {isLoaded, isSignedIn} = useUser()
+
+  if (!isLoaded){
+    return <div>Loading...</div>
+  }
+  
+  if(isLoaded && !isSignedIn){
+    return <div>You should Sign In</div>
+  }
+
   return (
-    <div className=''>Write</div>
+    <div className=''>
+      <h1>Create a new Post</h1>
+      <form action="">
+        <button>Add a cover image</button>
+        <input type="text" placeholder="My Awesome Story" />
+        <div>
+          <label htmlFor="">Choose a category:</label>
+          <select name="cat" id="">
+            <option value="general">General</option>
+            <option value="web-design">Web Design</option>
+            <option value="technology">Technology</option>
+            <option value="programming">Programming</option>
+            <option value="life-style">Life Style</option>
+            <option value="travel">Travel</option>
+          </select>
+        </div>
+        <textarea name="desc" id="" placeholder="A Short Description" />
+      </form>
+    </div>
   )
 }
 
